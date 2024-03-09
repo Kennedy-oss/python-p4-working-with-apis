9ed5308e605d7d061752637fd542acc3a6fbe4d1
Kennedy-oss <kennedy.onyango4@gmail.com>
2024-03-10 00:25:57 +0300

    Completed assignment

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,31 @@
-
 import requests
-import json
 
 class GetPrograms:
 
   def get_programs(self):
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
-
     response = requests.get(URL)
-    return response.content
+    data = response.json()  # Parse JSON response
+    if 'error' in data:
+        print("Error from API:", data.get('message'))
+    return data
 
-programs = GetPrograms().get_programs()
-print(programs)
+  def program_agencies(self):
+    programs = self.get_programs()
+    # If the programs variable is an error message, we won't try to process it further.
+    if 'error' in programs:
+        return []
+    
+    programs_list = []
+    # Additional processing here, adjusted based on the API's correct response structure
+
+    return programs_list
+
+programs = GetPrograms()
+agencies = programs.program_agencies()
+
+if agencies:
+    for agency in set(agencies):
+        print(agency)
+else:
+    print("No data to display due to API error.")
